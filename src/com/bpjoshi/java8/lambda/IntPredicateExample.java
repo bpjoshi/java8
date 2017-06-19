322090fa2c15239ba08b539233c32ee6a61f6775
bpjoshi <bpjoshi@bpjoshi.com>
2017-06-19 22:06:40 +0530

    Example of functional interface IntPredicate

---
+++ src/com/bpjoshi/java8/lambda/IntPredicateExample.java
@@ -0,0 +1,14 @@
+import java.util.function.IntPredicate;
+/*
+@author Bhagwati Prasad(bpjoshi)
+*/
+class IntPredicateExample {
+  public static void main(String[] args) {
+    IntPredicate evenNumbers=(int i)->i%2==0;
+    System.out.println("Is 10 an even number? "+evenNumbers.test(10));
+
+    IntPredicate oddNumbers=(int i)->i%2==1;
+    System.out.println("Is 12 an odd number? "+oddNumbers.test(12));
+  }
+
+}
