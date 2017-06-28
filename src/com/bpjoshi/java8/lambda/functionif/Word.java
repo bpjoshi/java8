f5cbfacb6a6d202ccceb493811384ab2fb6a0f7b
bpjoshi <bpjoshi@bpjoshi.com>
2017-06-28 16:29:44 +0530

    Method modification

--- src/com/bpjoshi/java8/lambda/functionif/Word.java
+++ src/com/bpjoshi/java8/lambda/functionif/Word.java
@@ -2,7 +2,7 @@ package com.bpjoshi.java8.lambda.functionif;
 
 public class Word {
 	public static String addHeader(String txt){
-		return "From Bhagwati Prasad with love :\n\n"+txt;
+		return "\nFrom Bhagwati Prasad with love :\n"+txt;
 	}
 	public static String addFooter(String txt){
 		return txt+"\n Kind Regards";
